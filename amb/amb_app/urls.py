b28e863d84e59d932af6ae8c27fe63d145829c45
Silas De Vuyst <silas@DESKTOP-HLO4E9K.localdomain>
2022-12-02 11:12:29 +0100

    Appointment is now stored in db

--- amb/amb_app/urls.py
+++ amb/amb_app/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('appointment', views.appointment, name='appointment'),
     re_path(r'^timeslots/(?P<date>[0-9]{4}-[0-9]{2}-[0-9]{2})$', views.getTimeSlots, name='getTimeSlots')
-    # path('timeslots', views.getTimeSlots, name='timeslots')
 ]
 
--- amb/amb_app/views.py
+++ amb/amb_app/views.py
@@ -3,7 +3,9 @@ from django.core.serializers import serialize
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import TimeSlot, Day
+import re
+
+from .models import TimeSlot, Day, Patient
 
 # Index view
 def index(request):
@@ -11,12 +13,35 @@ def index(request):
     return render(request, 'index.html', context)
 
 def appointment(request):
-    
+    # Nieuwe afspraak ingediend
     if request.method == 'POST':
-            # Your code for POST
-            # make sure to put a "return redirect" statement here
-        pass
+        # Check als data correct is
+        # Timeslot zit in de gekozen dag
+        
+        # make sure to put a "return redirect" statement here
+        date = request.POST.get("date", "")
+        date_cleaned = re.sub(r"([0-9]{2})\/([0-9]{2})\/([0-9]{4})", r"\3-\1-\2", date)
+        timeslot_pk = request.POST.get("timeslot", "")
+        first_name = request.POST.get("first_name", "")
+        last_name = request.POST.get("last_name", "")
 
+        DayObject = Day.objects.get(date=date_cleaned)
+        TimeslotObject = TimeSlot.objects.get(pk=timeslot_pk)
+        PatientObject = Patient(first_name=first_name, last_name=last_name)
+
+        # Timeslot zit in de gekozen dag
+        if TimeslotObject.day == DayObject:
+
+            # Save patient
+            PatientObject.save()        
+            # Add patient to timeslot
+            TimeslotObject.patient=PatientObject
+            TimeslotObject.save()
+        
+        print("Succes!")
+        return redirect('index')
+
+    # User wil nieuwe afspraak maken
     # all days where there is still an open timeslot
     daysSet = set()
     for slot in TimeSlot.objects.filter(available=True):
@@ -26,8 +51,8 @@ def appointment(request):
         'daysSet': daysSet
     })
 
+# Geeft de available timeslots terug voor een gegeven dag
 def getTimeSlots(request, date):
-    print(date)
     DayObject = Day.objects.get(date=date)
     queryset = TimeSlot.objects.filter(day=DayObject, available=True)
 
